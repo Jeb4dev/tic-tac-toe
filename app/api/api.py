ffe1cb3415dbfe1f813856876d978f1708d98110
JesperKauppinen <jesperkauppinen@gmail.com>
2022-01-22 14:46:47 +0200

    example api file from old project

---
+++ app/api/api.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+from flask_restful import Api
+from .resources import UserResource, LoginResource, LogoutResource, RefreshResource, RegisterResource, \
+    StatisticsResource, AccountResource
+
+router = Blueprint("api", __name__)
+api = Api(router)
+
+api.add_resource(UserResource, "/user/<int:user_id>")
+api.add_resource(LoginResource, "/auth/login")
+api.add_resource(LogoutResource, "/auth/logout")
+api.add_resource(RefreshResource, "/auth/refresh")
+api.add_resource(RegisterResource, "/auth/register")
+api.add_resource(StatisticsResource, "/statistics/<int:user_id>")
+api.add_resource(AccountResource, "/user/account")
